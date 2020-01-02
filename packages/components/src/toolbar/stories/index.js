a64ad0d1c2379f93c1b9fc75b780cf723a29a810
Riad Benguella <benguella@gmail.com>
2020-01-02 11:11:45 +0100

    no arrow indicator for dropdowns

--- packages/components/src/toolbar/stories/index.js
+++ packages/components/src/toolbar/stories/index.js
@@ -63,7 +63,6 @@ export const _default = () => {
 				/>
 			</ToolbarGroup>
 			<ToolbarGroup
-				hasArrowIndicator={ false }
 				icon="ellipsis"
 				label="Change text alignment"
 				isCollapsed
